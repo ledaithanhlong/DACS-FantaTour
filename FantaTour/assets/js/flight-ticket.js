7e9847d14d7c42a0c516e56909d5f82ab8ef094d
ledaithanhlong <lucaslee050304@gmail.com>
2025-04-02 05:32:43 +0700

    thêm trang đặt vé máy bay nhưng lỗi

---
+++ FantaTour/assets/js/flight-ticket.js
@@ -0,0 +1,30 @@
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
+
+
+const sliderImages = document.querySelector('.slider-images');
+const nextBtn = document.getElementById('next');
+const prevBtn = document.getElementById('prev');
+const slideWidth = 240; 
+let currentSlide = 0;
+
+
+nextBtn.addEventListener('click', () => {
+    if(currentSlide < sliderImages.children.length - 3){
+        currentSlide++;
+        updateSliderPosition();
+    }
+});
+
+prevBtn.addEventListener('click', () => {
+    if (currentSlide > 0) {
+        currentSlide--;
+        updateSliderPosition();
+    }
+});
+
+function updateSliderPosition() {
+    sliderImages.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+}
+
